93073007e3cabef637452aea8780adfb1165747d
Cainan Valle <128627178+Cainan17@users.noreply.github.com>
2024-03-25 20:06:06 -0300

    Add files via upload

---
+++ atividade 1/javascript/catalogo.js
@@ -0,0 +1,32 @@
+// DUPLA CAINAN, LUCAS SILVA
+
+const bot1 = document.querySelector('#bt1')
+const bot2 = document.querySelector('#bt2')
+const bot3 = document.querySelector('#bt3')
+const bot4 = document.querySelector('#bt4')
+const text = document.querySelector('#texto')
+
+bot1.addEventListener('click', bt1 )
+bot2.addEventListener('click', bt2 )
+bot3.addEventListener('click', bt3 )
+bot4.addEventListener('click', bt4 )
+
+
+
+
+function bt1(){
+imagem.src="img/barbie.webp"
+text.textContent="Eden Starling é uma glamourosa cantora de um teatro londrino. Acompanhada de seu gato Chuzzlewit, ela planeja fazer com que todos os artistas do local trabalhem durante o tão esperado dia de Natal. Observando tal atitude, Catherine, amiga de infância da diva, conversa sobre o seu mal-humor. Até que, em um momento mágico, três espíritos do Natal levam Eden a uma fantástica viagem de reflexões sobre o verdadeiro espírito do Natal."
+}
+function bt2(){
+imagem.src="img/percy.jpg"
+text.textContent="O adolescente Percy Jackson descobre que os deuses gregos e as criaturas mitológicas são reais. Ele é filho de uma divindade e logo entra para um acampamento de treinamento para semideuses. Enquanto tenta se adaptar a seus novos poderes e amigos, ele descobre que o Raio-Mestre do poderoso Zeus foi roubado e ele é o principal suspeito. Assim, ele tenta solucionar o mistério junto com seus novos colegas, Grover e Annabeth."
+}
+function bt3(){
+imagem.src="img/harry.webp"
+text.textContent="Harry Potter é um garoto órfão que vive infeliz com seus tios, os Dursleys. Ele recebe uma carta contendo um convite para ingressar em Hogwarts, uma famosa escola especializada em formar jovens bruxos. Inicialmente, Harry é impedido de ler a carta por seu tio, mas logo recebe a visita de Hagrid, o guarda-caça de Hogwarts, que chega para levá-lo até a escola. Harry adentra um mundo mágico que jamais imaginara, vivendo diversas aventuras com seus novos amigos, Rony Weasley e Hermione Granger."
+}
+function bt4(){
+imagem.src="img/Bullet.jpg"
+text.textContent="Em um trem-bala indo rapidamente de Tóquio a Morioka, cinco assassinos profissionais descobrem que possuem o mesmo objetivo."
+}
